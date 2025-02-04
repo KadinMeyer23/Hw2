7f205a93c4ba8a7c4f0ffaf2c31ee5a744271bd8
KadinMeyer23 <vq4717wg@go.minnstate.edu>
2025-02-04 10:41:52 -0600

    Add files via upload

---
+++ Hw2 Assignments/BuyingInventory/Program.cs
@@ -0,0 +1,26 @@
+﻿//Written By Kadin Meyer
+// 2/4/2025
+
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Diagnostics;
+using System.Reflection.PortableExecutable;
+using System.Runtime.Intrinsics.X86;
+using System;
+
+namespace BuyingInventory
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("The following items are available:\n 1 – Rope \n 2 – Torches\n 3 – Climbing Equipment\n 4 – Clean Water\n 5 – Machete \n 6 – Canoe\n 7 – Food Supplies\n What number do you want to see the price of?");
+            int item = Convert.ToInt32(Console.ReadLine());
+            
+            List<Shop> listOfItems = new List<Shop>();
+            Shop ashop = new Shop(item);
+            listOfItems.Add(ashop);
+            ashop.GetCategory();
+        }
+    }
+}
