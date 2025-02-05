af8ad6377cd18d35714fce02b0c24791398705b8
KadinMeyer23 <vq4717wg@go.minnstate.edu>
2025-02-05 11:32:54 -0600

    Add files via upload

---
+++ MagicCannon/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace MagicCannon
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           List<Warning> listofwarnings = new List<Warning>();
+            Warning awarning = new Warning();
+            listofwarnings.Add(awarning);
+
+            awarning.warner();
+        }
+    }
+}
